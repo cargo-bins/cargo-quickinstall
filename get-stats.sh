3211964d2c727c1a6bcdfc7be2c1743ee332ae3b
David Laban <david.laban@red-badger.com>
2020-11-24 18:04:32 +0000

    switch to body-based elasticsearch api
    following this guide: https://elasticsearch-cheatsheet.jolicode.com/

--- get-stats.sh
+++ get-stats.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
+set -euo pipefail
 
 source .env
 
 # Uses https://sematext.com/docs/logs/search-through-the-elasticsearch-api/
 # TODO:
-# * Switch to using the body-based API.
 # * Add a time range to the query.
 # * Add aggregation to the query.
-curl -u "apiKey:${SEMATEXT_API_KEY}" -XGET "logsene-receiver.sematext.com/${SEMATEXT_APP_TOKEN}/_search?pretty&q=*"
+
+QUERY=$(echo '
+{
+  "query": {
+    "query_string": {
+      "query": "*"
+    }
+  }
+}
+' | jq)
+echo "$QUERY"
+
+curl -u "apiKey:${SEMATEXT_API_KEY}" -XGET "logsene-receiver.sematext.com/${SEMATEXT_APP_TOKEN}/_search?pretty" -d "$QUERY"
