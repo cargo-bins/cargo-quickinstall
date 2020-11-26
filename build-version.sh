b45c26fe35b05d56c75028b40ba4fe81f999a778
David Laban <david.laban@red-badger.com>
2020-11-26 11:45:39 +0000

    idempotent package creation and file upload

--- build-version.sh
+++ build-version.sh
@@ -20,6 +20,14 @@ BINARIES=$(
     '
 )
 
+# FIXME: fetch the latest $VERSION from crates.io, and do this check early, before we've even thought about
+# running `cargo install`
+# FIXME: this filename wants to include the target triple or something.
+if curl --fail -I --output /dev/null "https://dl.bintray.com/cargo-quickinstall/cargo-quickinstall/${CRATE}-${VERSION}.tar.gz"; then
+    echo "${CRATE}/${VERSION}/${CRATE}-${VERSION}.tar.gz already uploaded. Skipping."
+    exit 0
+fi
+
 TEMPDIR=$(mktemp -d)
 
 echo "$VERSION"
@@ -34,18 +42,48 @@ tar -czf "${TEMPDIR}/${CRATE}-${VERSION}.tar.gz" $BINARIES
 
 echo "${TEMPDIR}/${CRATE}-${VERSION}.tar.gz"
 
-# TODO: find a way to programatically make a new package with name ${CRATE}.
-# How does homebrew do it?
+CURL='curl --fail --location -w \n'
+
+function curl_better() {
+    if ! curl --fail --location -w '\n' "$@"; then
+        echo "curl failed. Retrying for debug information and then exiting."
+        curl --location -w '\n' "$@"
+        false
+    fi
+}
+
+if ! $CURL --output /dev/null "https://api.bintray.com/packages/cargo-quickinstall/cargo-quickinstall/${CRATE}"; then
+    echo "package '${CRATE}' not found"
+
+    curl --fail "https://crates.io/api/v1/crates/${CRATE}" >"$TEMPDIR/crates.io-response.json"
+
+    LICENSE=$(cat "$TEMPDIR/crates.io-response.json" | jq -r .versions[0].license)
+    REPOSITORY=$(cat "$TEMPDIR/crates.io-response.json" | jq -r .crate.repository)
+
+    curl_better \
+        --user "${BINTRAY_USERNAME}:${BINTRAY_API_KEY}" \
+        --header "Content-Type: application/json" \
+        --data '
+            {
+                "name": "'${CRATE}'",
+                "public_download_numbers": true,
+                "public_stats": true,
+                "vcs_url": "'${REPOSITORY}'",
+                "licenses": ["'${LICENSE}'"]
+            }' \
+        "https://api.bintray.com/packages/cargo-quickinstall/cargo-quickinstall"
+fi
+
 echo "uploading file"
-curl \
+$CURL \
     --upload-file "${TEMPDIR}/${CRATE}-${VERSION}.tar.gz" \
-    -u"${BINTRAY_USERNAME}:${BINTRAY_API_KEY}" \
+    --user "${BINTRAY_USERNAME}:${BINTRAY_API_KEY}" \
     "https://api.bintray.com/content/cargo-quickinstall/cargo-quickinstall/${CRATE}/${VERSION}/${CRATE}-${VERSION}.tar.gz"
 
 echo "publishing version"
-curl \
+$CURL \
     -X POST \
-    -u"${BINTRAY_USERNAME}:${BINTRAY_API_KEY}" \
+    --user "${BINTRAY_USERNAME}:${BINTRAY_API_KEY}" \
     "https://api.bintray.com/content/cargo-quickinstall/cargo-quickinstall/${CRATE}/${VERSION}/publish"
 
 rm -r "$TEMPDIR"
