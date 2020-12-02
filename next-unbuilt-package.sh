a20c70cacc5ccf07b9c0e7eaabfe8c95e1c49671
David Laban <david.laban@red-badger.com>
2020-12-02 11:05:59 +0000

    actually build something in cronjob

--- next-unbuilt-package.sh
+++ next-unbuilt-package.sh
@@ -3,11 +3,14 @@
 for crate in $(
   # FIXME: find a better naming scheme for this so that it can be parsed more easily
   ./get-stats.sh |
-      jq -r '.aggregations.my_agg.buckets | map(.key)| .[]' |
-      sed -n 's:^/api/crate/::p' |
-      sed 's/-[0-9].*$//'
+    jq -r '.aggregations.my_agg.buckets | map(.key)| .[]' |
+    sed -n 's:^/api/crate/::p' |
+    sed 's/-[0-9].*$//'
 ); do
   if ./needs-version.sh "$crate" 1>&2; then
     echo "$crate"
+    exit 0
   fi
 done
+# If there's nothing to build, just build ourselves.
+echo "cargo-quickinstall"
