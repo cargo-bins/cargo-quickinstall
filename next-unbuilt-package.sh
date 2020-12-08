0c93e520065ebca3c1dd69209e895e1268327671
David Laban <david.laban@red-badger.com>
2020-12-08 16:14:11 +0000

    hardcode a list of popular crates for now

--- next-unbuilt-package.sh
+++ next-unbuilt-package.sh
@@ -2,13 +2,122 @@
 
 set -euxo pipefail
 
-for CRATE in $(
-  # FIXME: find a better naming scheme for this so that it can be parsed more easily
-  ./get-stats.sh |
-    jq -r '.aggregations.my_agg.buckets | map(.key)| .[]' |
-    sed -n 's:^/api/crate/::p' |
-    sed 's/-[0-9].*$//'
-); do
+# To re-generate this list, run:
+#     curl https://lib.rs/command-line-utilities > cli.html
+#     cat cli.html |
+#         pup ':parent-of(:parent-of(:parent-of(.bin))) json{}' |
+#         jq -r '.[] |
+#             (.children[1].children|map(select(.class == "downloads").title)[0]// "0 ")
+#             + ":" +
+#             (.children[0].children[0].text)' |
+#         sort -gr |
+#         head -n 100 |
+#         sed s/^.*://
+# and then audit any new entries.
+POPULAR_CRATES="
+y2z/monolith
+ogham/dog
+tanakh/cargo-atcoder
+bat
+benschza/pier
+cargo-bump
+watchexec
+ripgrep
+just
+tokei
+exa
+starship
+fd-find
+kbknapp/docli
+cargo-binutils
+skim
+gptman
+streampager
+rustdoc-stripper
+du-dust
+procs
+broot
+petname
+bottom
+dijo
+elf2tab
+intel-mkl-tool
+lsd
+bandwhich
+sd
+gitui
+project_init
+hyperfine
+scryer-prolog
+rsign2
+wrangler
+tealdeer
+tin-summer
+nj-cli
+zoxide
+cargo-rpm
+mdcat
+rustscan
+rust-latest
+jql
+hexyl
+git-delta
+sgxs-tools
+spotify-tui
+fw
+routinator
+my-iot
+critcmp
+yaksay
+rusty-tags
+b3sum
+dutree
+tp-note
+ffsend
+git_lab_cli
+viu
+dotenv-linter
+feroxbuster
+sane-fmt
+podcast
+ff-find
+cargo-trim
+kmon
+file-sniffer
+grex
+so
+what-bump
+xcompress
+tab
+dtn7
+code-minimap
+nitrocli
+fblog
+rexcli
+zdump
+navi
+verco
+svlint
+emplace
+spl-token-cli
+bacon
+yatt
+stack-sizes
+mdblog
+complate
+phetch
+kurobako
+flowy
+rust-covfix
+cobalt-bin
+kickstart
+gitoxide
+otpcli
+i2p_client
+sniffglue
+"
+
+for CRATE in $POPULAR_CRATES; do
 
   VERSION=$(curl --location --fail "https://crates.io/api/v1/crates/${CRATE}" | jq -r .versions[0].num)
   TARGET_ARCH=$(rustc --version --verbose | sed -n 's/host: //p')
