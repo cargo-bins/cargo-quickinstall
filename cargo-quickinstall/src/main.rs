89b666e30d2747e4e029e39e9252c1c5ed08718d
David Laban <david.laban@red-badger.com>
2020-12-14 12:45:49 +0000

    make my own error type

--- cargo-quickinstall/src/main.rs
+++ cargo-quickinstall/src/main.rs
@@ -8,31 +8,67 @@
 // the bootstrapping time too much. Patches to do this would be very welcome.
 
 use std::convert::TryInto;
-use std::io::{Error, ErrorKind};
+use std::io::ErrorKind;
 use tinyjson::JsonValue;
 
-fn bash_stdout(command_string: &str) -> std::io::Result<String> {
-    let command_string = format!("set -euo pipefail && {}", command_string);
+#[derive(Debug)]
+enum InstallError {
+    CommandFailed {
+        command: String,
+        stdout: String,
+        stderr: String,
+    },
+    IoError(std::io::Error),
+}
+
+impl From<std::io::Error> for InstallError {
+    fn from(err: std::io::Error) -> InstallError {
+        InstallError::IoError(err)
+    }
+}
+impl std::error::Error for InstallError {}
+
+impl std::fmt::Display for InstallError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            InstallError::CommandFailed {
+                command,
+                stdout,
+                stderr,
+            } => write!(
+                f,
+                "Command failed:\n    {}\nStdout:\n{}\nStderr:\n{}",
+                command, stdout, stderr
+            ),
+            InstallError::IoError(e) => write!(f, "{}", e),
+        }
+    }
+}
+
+fn bash_stdout(command: &str) -> Result<String, InstallError> {
+    let command_string = format!("set -euo pipefail && {}", command);
     let output = std::process::Command::new("bash")
         .arg("-c")
         .arg(&command_string)
         .output()?;
 
-    if !output.status.success() {
-        println!("{:?} => {:#?}", command_string, output);
-        return Err(std::io::Error::new(
-            std::io::ErrorKind::Other,
-            "Command failed",
-        ));
-    }
-
     let mut stdout = String::from_utf8(output.stdout).unwrap();
     let len = stdout.trim_end_matches('\n').len();
     stdout.truncate(len);
+
+    if !output.status.success() {
+        let stderr = String::from_utf8(output.stderr).unwrap();
+        return Err(InstallError::CommandFailed {
+            command: command.to_string(),
+            stdout,
+            stderr,
+        });
+    }
+
     Ok(stdout)
 }
 
-fn get_latest_version(crate_name: &str) -> std::io::Result<String> {
+fn get_latest_version(crate_name: &str) -> Result<String, InstallError> {
     let command_string = format!(
         "curl \
             --user-agent 'cargo-quickinstall build pipeline (alsuren@gmail.com)' \
@@ -43,11 +79,11 @@ fn get_latest_version(crate_name: &str) -> std::io::Result<String> {
     );
     let parsed: JsonValue = bash_stdout(&command_string)?
         .parse()
-        .map_err(|_| Error::new(ErrorKind::InvalidData, "Unable to parse JSON."))?;
+        .map_err(|_| std::io::Error::new(ErrorKind::InvalidData, "Unable to parse JSON."))?;
     Ok(parsed["versions"][0]["num"].clone().try_into().unwrap())
 }
 
-fn get_target_triple() -> std::io::Result<String> {
+fn get_target_triple() -> Result<String, InstallError> {
     // Credit to https://stackoverflow.com/a/63866386
     bash_stdout("rustc --version --verbose | sed -n 's/host: //p'")
 }
@@ -69,7 +105,7 @@ fn report_stats_in_background(
     })
 }
 
-fn install_crate(crate_name: &str, version: &str, target: &str) -> std::io::Result<()> {
+fn install_crate(crate_name: &str, version: &str, target: &str) -> Result<(), InstallError> {
     let tarball_name = format!("{}-{}-{}.tar.gz", crate_name, version, target);
 
     let download_url = format!(
@@ -77,7 +113,7 @@ fn install_crate(crate_name: &str, version: &str, target: &str) -> std::io::Resu
         tarball_name
     );
     let install_command = format!(
-        "curl --location --fail '{}' | tar -xzvvf - -C ~/.cargo/bin 2>&1",
+        "curl --silent --show-error --location --fail '{}' | tar -xzvvf - -C ~/.cargo/bin 2>&1",
         download_url
     );
     match bash_stdout(&install_command) {
@@ -86,10 +122,8 @@ fn install_crate(crate_name: &str, version: &str, target: &str) -> std::io::Resu
             crate_name, version, tar_output
         ),
         Err(err) => {
-            println!(
-                "Got {:?} when trying to install. Falling back to `cargo install`.",
-                err
-            );
+            println!("{}", err);
+            println!("Falling back to `cargo install`.");
             println!("We have reported your installation request, so it should be built soon.");
 
             if !std::process::Command::new("cargo")
