8d667a9b7f317bb40031d2530937c3db760da72c
David Laban <alsuren@gmail.com>
2021-04-04 13:55:02 +0100

    delete tag when broken file is found

--- check-packages.sh
+++ check-packages.sh
@@ -17,9 +17,13 @@ function check_packages() {
             cat "$TEMPDIR/$tag.tar.gz" | tar -xzf - -C "$TEMPDIR/$tag"
             rm "$TEMPDIR/$tag.tar.gz"
         fi
-        for file in $TEMPDIR/$tag/*; do
-            file $file | grep ': data$' || exitcode=1
+        for file in "$TEMPDIR/$tag/"*; do
+            if file "$file" | grep ': data$'; then
+                exitcode=1
+                git push --delete origin "$tag" || true
+            fi
         done
+
         # rm -r "${TEMPDIR:?}/$tag"
     done
     exit $exitcode
