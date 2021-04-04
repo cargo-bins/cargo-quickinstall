4757ec1d33b605c01817108a450a0fd27283388c
David Laban <alsuren@gmail.com>
2021-04-04 12:54:36 +0100

    check that binaries for macos are valid

--- build-version.sh
+++ build-version.sh
@@ -37,11 +37,19 @@ BINARIES=$(
     '
 )
 
+
+cd ~/.cargo/bin
+for file in $BINARIES; do
+    if file $file | grep ': data$'; then
+        echo "something wrong with $file. Should be recognised as executable."
+        exit 1
+    fi
+done
+
 # Package up the binaries so that they can be untarred in ~/.cargo/bin
 #
 # TODO: maybe we want to make a ~/.cargo-quickinstall/bin to untar into,
 # and add symlinks into ~/.cargo/bin, to aid debugging?
-cd ~/.cargo/bin
 tar -czf "${TEMPDIR}/${CRATE}-${VERSION}-${TARGET_ARCH}.tar.gz" $BINARIES
 
 echo "${TEMPDIR}/${CRATE}-${VERSION}-${TARGET_ARCH}.tar.gz"
---
+++ check-packages.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# shellcheck disable=SC2002
+set -euo pipefail
+cd "$(dirname "$0")"
+
+GITHUB="https://github.com/alsuren/cargo-quickinstall/releases/download"
+TEMPDIR=/tmp/check-packages
+mkdir -p "$TEMPDIR"
+
+function check_packages() {
+    exitcode=0
+    for tag in $(git tag | grep x86_64-apple-darwin); do
+        # echo "$tag"
+        if [[ ! -d "$TEMPDIR/$tag" ]]; then
+            curl --silent --location --fail "${GITHUB}/${tag}/${tag}.tar.gz" > "$TEMPDIR/$tag.tar.gz"
+            mkdir "$TEMPDIR/$tag"
+            cat "$TEMPDIR/$tag.tar.gz" | tar -xzf - -C "$TEMPDIR/$tag"
+            rm "$TEMPDIR/$tag.tar.gz"
+        fi
+        for file in $TEMPDIR/$tag/*; do
+            file $file | grep ': data$' || exitcode=1
+        done
+        # rm -r "${TEMPDIR:?}/$tag"
+    done
+    exit $exitcode
+}
+
+check_packages
