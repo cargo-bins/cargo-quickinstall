998dc56cc522033f99fe78f509a0803e01564845
David Laban <alsuren@gmail.com>
2022-02-26 14:40:02 +0000

    document historical stats api

--- get-stats.sh
+++ get-stats.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -euo pipefail
 
+# Note that it is also possible to get stats from previous months via
+# /api/stats?year=2022&month=1
+
 curl \
   --user-agent "cargo-quickinstall build pipeline (alsuren@gmail.com)" \
   --silent \
