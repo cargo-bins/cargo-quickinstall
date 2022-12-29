711142052bb60b2b0d27a6233fe3ef92509d8228
David Laban <david@opvia.bio>
2022-12-29 11:30:46 +0000

    write in tidy jsonl format as well
    See https://plotly.com/python/wide-form/

--- stats/fetch.py
+++ stats/fetch.py
@@ -7,6 +7,14 @@ from urllib.request import urlopen
 import typing
 
 
+class DownloadStats(typing.TypedDict):
+    day: str
+    package: str
+    version: str
+    arch: str
+    count: int
+
+
 def fetch_one_day_of_stats(day: datetime.date) -> typing.Dict[str, int]:
     here = os.path.dirname(os.path.realpath(__file__))
     filename = f"{here}/raw/{day.isoformat()}.json"
@@ -22,20 +30,45 @@ def fetch_one_day_of_stats(day: datetime.date) -> typing.Dict[str, int]:
         return json.load(f)
 
 
-def fetch_all_stats():
-    # Don't get today's data because it might still be being written.
-    # Also don't get yesterday, because we might be in a timezone
-    # that is ahead of UTC, so it might still be being written.
-    day = datetime.date.today() - datetime.timedelta(days=2)
+def tidy_stats(day: datetime.date, stats: typing.Dict[str, int]) -> typing.Iterable[DownloadStats]:
+    for k, count in stats.items():
+        (package, version, arch) = k.split("/")
 
+        yield {
+            "date": day.isoformat(),
+            "package": package,
+            "version": version,
+            "arch": arch,
+            "count": count
+        }
+
+
+def fetch_all_stats() -> typing.Iterable[DownloadStats]:
     # We switched from monthly to daily on datetime.date(2022, 7, 30)
     # but this will do for now: don't want to blast through our upstash
     # quotas in one go.
-    while day >= datetime.date(2022, 9, 1):
+    earliest = datetime.date(2022, 9, 1)
+    # Don't get today's data because it might still be being written.
+    # Also don't get yesterday, because we might be in a timezone
+    # that is ahead of UTC, so it might still be being written.
+    latest = datetime.date.today() - datetime.timedelta(days=2)
+
+    day = earliest
+    while day <= latest:
         stats = fetch_one_day_of_stats(day)
         assert stats, "stats dict should not be empty"
-        day -= datetime.timedelta(days=1)
+        yield from tidy_stats(day, stats)
+        day += datetime.timedelta(days=1)
+
+
+def write_tidied_stats():
+    here = os.path.dirname(os.path.realpath(__file__))
+    filename = f"{here}/tidy.jsonl"
+    with open(filename, 'w') as f:
+        for stat in fetch_all_stats():
+            json.dump(stat, f)
+            f.write('\n')
 
 
 if __name__ == "__main__":
-    fetch_all_stats()
+    write_tidied_stats()
