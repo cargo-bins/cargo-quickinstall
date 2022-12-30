fd6133e0c6ae0fe044eaa20027c997861b289538
David Laban <david@opvia.bio>
2022-12-30 16:31:05 +0000

    also add build date

--- stats/fetch.py
+++ stats/fetch.py
@@ -6,13 +6,16 @@ import os
 import subprocess
 import typing
 
-
-class DownloadStats(typing.TypedDict):
+class DownloadRecord(typing.TypedDict):
     day: str
     package: str
     version: str
     arch: str
     count: int
+
+
+class FullRecord(DownloadRecord):
+    build_date: str | None
 
 
 def fetch_one_day_of_stats(day: datetime.date) -> typing.Dict[str, int]:
@@ -34,7 +37,7 @@ def fetch_one_day_of_stats(day: datetime.date) -> typing.Dict[str, int]:
         return json.load(f)
 
 
-def tidy_stats(day: datetime.date, stats: typing.Dict[str, int]) -> typing.Iterable[DownloadStats]:
+def tidy_stats(day: datetime.date, stats: typing.Dict[str, int]) -> typing.Iterable[DownloadRecord]:
     for k, count in stats.items():
         (package, version, arch) = k.split("/")
 
@@ -46,8 +49,35 @@ def tidy_stats(day: datetime.date, stats: typing.Dict[str, int]) -> typing.Itera
             "count": count
         }
 
+_build_date_cache = {}
 
-def fetch_all_stats() -> typing.Iterable[DownloadStats]:
+def get_build_date(record: DownloadRecord) -> str | None:
+    tag = f'{record["package"]}-{record["version"]}-{record["arch"]}'
+    if tag in _build_date_cache:
+        return _build_date_cache[tag]
+
+    try:
+        build_date = subprocess.check_output([
+            "git",
+            "show",
+            "--quiet",
+            "--pretty=%cd",  # committer date
+            "--date=short",
+            tag,
+        ], stderr=subprocess.DEVNULL).strip().decode()
+    except subprocess.CalledProcessError:
+        build_date = None
+
+    _build_date_cache[tag] = build_date
+    return build_date
+
+
+def add_build_date(record: DownloadRecord) -> FullRecord:
+    build_date = get_build_date(record)
+    return dict(**record, build_date=build_date)
+
+
+def fetch_all_records() -> typing.Iterable[DownloadRecord]:
     # We switched from monthly to daily on datetime.date(2022, 7, 30)
     # but this will do for now: don't want to blast through our upstash
     # quotas in one go.
@@ -61,18 +91,19 @@ def fetch_all_stats() -> typing.Iterable[DownloadStats]:
     while day <= latest:
         stats = fetch_one_day_of_stats(day)
         assert stats, "stats dict should not be empty"
-        yield from tidy_stats(day, stats)
+        for record in tidy_stats(day, stats):
+            yield add_build_date(record)
         day += datetime.timedelta(days=1)
 
 
-def write_tidied_stats():
+def write_tidied_records():
     here = os.path.dirname(os.path.realpath(__file__))
     filename = f"{here}/tidy.jsonl"
     with open(filename, 'w') as f:
-        for stat in fetch_all_stats():
+        for stat in fetch_all_records():
             json.dump(stat, f)
             f.write('\n')
 
 
 if __name__ == "__main__":
-    write_tidied_stats()
+    write_tidied_records()
