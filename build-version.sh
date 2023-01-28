ba82d45b93c1d5989a32531e71b9b0adf421962a
Milo <50248166+Milo123459@users.noreply.github.com>
2023-01-28 20:24:45 +0000

    thisll hopefully unzip

--- build-version.sh
+++ build-version.sh
@@ -33,7 +33,7 @@ if [ "$TARGET_ARCH" == "x86_64-unknown-linux-musl" ]; then
 elif [ "$TARGET_ARCH" == "aarch64-unknown-linux-gnu" ]; then
     wget "$(curl -q https://ziglang.org/download/index.json | jq 'to_entries | map([.key, .value])[1][1]["x86_64-linux"] | .tarball' | sed -e 's/^"//' -e 's/"$//')" -O zig
     wget https://dl.google.com/android/repository/android-ndk-r25b-linux.zip
-    sudo unzip ndk
+    sudo unzip android-ndk-r25b-linux.zip
     cd android-ndk-r25b-linux/android-ndkr25b/toolchains/llvm/prebuilt/linux-x86_64/bin
     sudo chmod +x ./ld
     sudo mv ./ld /usr/bin
