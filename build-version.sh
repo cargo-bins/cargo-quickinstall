bbe2d159009e8b15037cf6c4017c2d3b9265ed5e
Milo <50248166+Milo123459@users.noreply.github.com>
2023-01-28 20:30:43 +0000

    yay

--- build-version.sh
+++ build-version.sh
@@ -33,10 +33,13 @@ if [ "$TARGET_ARCH" == "x86_64-unknown-linux-musl" ]; then
 elif [ "$TARGET_ARCH" == "aarch64-unknown-linux-gnu" ]; then
     wget "$(curl -q https://ziglang.org/download/index.json | jq 'to_entries | map([.key, .value])[1][1]["x86_64-linux"] | .tarball' | sed -e 's/^"//' -e 's/"$//')" -O zig
     wget https://dl.google.com/android/repository/android-ndk-r25b-linux.zip -O ndk
-    sudo unzip ndk
+    sudo unzip ndk -q
+    PREVIOUS_PWD=$PWD
     cd android-ndk-r25b/toolchains/llvm/prebuilt/linux-x86_64/bin
     sudo chmod +x ./ld
     sudo mv ./ld /usr/bin
+    cd $PREVIOUS_PWD
+    rm -rf android-ndk-r25b
     mkdir zigfolder
     tar -xf ./zig -C zigfolder --strip-components 1
     rm zig
