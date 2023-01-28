a491d2d08be039bfd1ba9134fb952aa23356226c
Milo <50248166+Milo123459@users.noreply.github.com>
2023-01-28 22:12:22 +0000

    if statement

--- build-version.sh
+++ build-version.sh
@@ -39,8 +39,10 @@ elif [ "$TARGET_ARCH" == "aarch64-unknown-linux-gnu" ]; then
     sudo mv zigfolder/zig /usr/local/bin/zig
     sudo mv zigfolder/lib /usr/local/bin/lib
     rustup target add "$TARGET_ARCH"
-    echo "[target.aarch64-unknown-linux-gnu]" >>~/.cargo/config
-    echo 'linker = "/home/runner/work/cargo-quickinstall/cargo-quickinstall/zig-aarch64.sh"' >>~/.cargo/config
+    if ! [ -f "~/.cargo/config" ] then;
+        echo "[target.aarch64-unknown-linux-gnu]" >>~/.cargo/config
+        echo 'linker = "/home/runner/work/cargo-quickinstall/cargo-quickinstall/zig-aarch64.sh"' >>~/.cargo/config
+    fi
 
     CARGO_PROFILE_RELEASE_CODEGEN_UNITS="1" CARGO_PROFILE_RELEASE_LTO="fat" OPENSSL_STATIC=1 CC=./zig-aarch64.sh cargo install "$CRATE" --version "$VERSION" --target "$TARGET_ARCH"
     CARGO_BIN_DIR=~/.cargo/bin
