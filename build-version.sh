68644613827b2c4915cd8cc58eeec187d27b23ba
David Laban <alsuren@gmail.com>
2023-01-28 23:41:13 +0000

    make paths consistent; survive things being different

--- build-version.sh
+++ build-version.sh
@@ -31,10 +31,7 @@ if [ "$TARGET_ARCH" == "x86_64-unknown-linux-musl" ]; then
     CARGO_BIN_DIR="${TEMPDIR}/cargo/bin"
     CRATES2_JSON_PATH="${TEMPDIR}/cargo/.crates2.json"
 elif [ "$TARGET_ARCH" == "aarch64-unknown-linux-gnu" ]; then
-    echo $PWD
-    ls $PWD
-    tree /home/runner/work/cargo-quickinstall
-    ls -l /home/runner/work/cargo-quickinstall/cargo-quickinstall/zig-aarch64.sh
+    ls $PWD/zig-aarch64.sh
 
     mkdir -p zigfolder
     curl "$(curl -q https://ziglang.org/download/index.json | jq 'to_entries | map([.key, .value])[1][1]["x86_64-linux"] | .tarball' | sed -e 's/^"//' -e 's/"$//')" | tar -xJ -C zigfolder --strip-components 1
@@ -43,7 +40,7 @@ elif [ "$TARGET_ARCH" == "aarch64-unknown-linux-gnu" ]; then
     rustup target add "$TARGET_ARCH"
     if ! [ -f "$HOME/.cargo/config" ]; then
         echo "[target.aarch64-unknown-linux-gnu]" >>~/.cargo/config
-        echo 'linker = "/home/runner/work/cargo-quickinstall/cargo-quickinstall/zig-aarch64.sh"' >>~/.cargo/config
+        echo "linker = \"$PWD/zig-aarch64.sh\"" >>~/.cargo/config
     fi
 
     CARGO_PROFILE_RELEASE_CODEGEN_UNITS="1" CARGO_PROFILE_RELEASE_LTO="fat" OPENSSL_STATIC=1 CC=./zig-aarch64.sh cargo install "$CRATE" --version "$VERSION" --target "$TARGET_ARCH"
