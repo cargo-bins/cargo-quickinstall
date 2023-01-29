79ec3721354634893c0c9f901c3d019fb2bb6abe
Milo <50248166+Milo123459@users.noreply.github.com>
2023-01-29 09:59:30 +0000

    shfmt

--- build-version.sh
+++ build-version.sh
@@ -41,7 +41,7 @@ elif [ "$TARGET_ARCH" == "aarch64-unknown-linux-gnu" ]; then
         echo "linker = \"$PWD/zig-aarch64-gnu.sh\"" >>~/.cargo/config
     fi
 
-    rm -f ~/.cargo/.crates.toml ~/.cargo/.crates2.json 
+    rm -f ~/.cargo/.crates.toml ~/.cargo/.crates2.json
     CARGO_PROFILE_RELEASE_CODEGEN_UNITS="1" CARGO_PROFILE_RELEASE_LTO="fat" OPENSSL_STATIC=1 CC=./zig-aarch64-gnu.sh cargo auditable install "$CRATE" --version "$VERSION" --target "$TARGET_ARCH"
     CARGO_BIN_DIR=~/.cargo/bin
     CRATES2_JSON_PATH=~/.cargo/.crates2.json
