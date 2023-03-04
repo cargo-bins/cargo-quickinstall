b80fcbfa361d9eb92bffe05f89ab4f2dd11f6d90
David Laban <alsuren@gmail.com>
2023-03-04 20:29:04 +0000

    fixup! import skeleton from https://github.com/fly-apps/hello-rust

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    // Match any request and return hello world!
+    let routes = warp::any().map(|| "Hello, World!");
+
+    warp::serve(routes)
+        // ipv6 + ipv6 any addr
+        .run(([0, 0, 0, 0, 0, 0, 0, 0], 8080))
+        .await;
 }
