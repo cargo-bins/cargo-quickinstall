3aa6eeed72c5607e872982d18e753133f6bff9a4
David Laban <alsuren@gmail.com>
2023-03-04 22:44:48 +0000

    switch to axum, skeleton /record-install handler

--- server/fly.io/src/main.rs
+++ server/fly.io/src/main.rs
@@ -1,12 +1,30 @@
-use warp::Filter;
+use axum::{
+    extract::Query,
+    routing::{get, post},
+    Router,
+};
+use std::collections::HashMap;
+use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
-    // Match any request and return hello world!
-    let routes = warp::any().map(|| "Hello, World!");
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/record-install", get(record_install))
+        .route("/record-install", post(record_install));
 
-    warp::serve(routes)
-        // ipv6 + ipv6 any addr
-        .run(([0, 0, 0, 0, 0, 0, 0, 0], 8080))
-        .await;
+    // ipv6 + ipv6 any addr
+    let addr = SocketAddr::from(([0, 0, 0, 0, 0, 0, 0, 0], 8080));
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn root() -> &'static str {
+    "Hello, World!"
+}
+
+async fn record_install(Query(params): Query<HashMap<String, String>>) -> String {
+    format!("Hi there {params:?}")
 }
