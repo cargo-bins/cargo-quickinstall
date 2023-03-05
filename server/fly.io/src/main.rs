464be77711afc9d499c0cd1fd3bae167cc8c3f90
David Laban <alsuren@gmail.com>
2023-03-05 10:06:07 +0000

    WIP using influx_db_client (authentication error)

--- server/fly.io/src/main.rs
+++ server/fly.io/src/main.rs
@@ -1,10 +1,12 @@
+use std::collections::BTreeMap;
+use std::net::SocketAddr;
+
 use axum::{
     extract::Query,
     routing::{get, post},
     Router,
 };
-use std::collections::BTreeMap;
-use std::net::SocketAddr;
+use influx_db_client::{reqwest::Url, Client, Point};
 
 #[tokio::main]
 async fn main() {
@@ -25,7 +27,47 @@ async fn root() -> &'static str {
     "Hello, World!"
 }
 
+fn get_env(key: &str) -> String {
+    std::env::var(key).expect(&format!("{key} must be set"))
+}
+
 async fn record_install(Query(params): Query<BTreeMap<String, String>>) -> String {
     println!("Hi there {params:?}");
+
+    // FIXME: make this in main and pass it down or something?
+    let url = Url::parse(get_env("INFLUX_URL").as_str()).unwrap();
+    let bucket = get_env("INFLUX_BUCKET");
+    let org = get_env("INFLUX_ORG");
+    let token = get_env("INFLUX_TOKEN");
+    let client = Client::new(url, bucket).set_jwt_token(token);
+
+    let mut point = Point::new("counts").add_field("count", 1);
+    for (tag, value) in &params {
+        point = point.add_tag(tag, &**value)
+    }
+    client.write_point(point, None, None).await.unwrap();
     format!("Hi there {params:?}")
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::BTreeMap;
+
+    use axum::extract::Query;
+
+    use crate::record_install;
+
+    #[tokio::test]
+    async fn smoke_test_against_real_server() {
+        if std::env::var("INFLUX_TOKEN").is_err() {
+            println!("set INFLUX_URL, INFLUX_ORG and INFLUX_TOKEN to enable this test");
+            return;
+        }
+        record_install(Query(
+            [("x".to_string(), "y".to_string())]
+                .into_iter()
+                .collect::<BTreeMap<String, String>>(),
+        ))
+        .await;
+    }
 }
