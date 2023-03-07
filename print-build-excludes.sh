e78390ead05028348dafdae1e87f18c984f409c0
Jiahao XU <Jiahao_XU@outlook.com>
2023-03-07 23:48:45 +1100

    Improve `print-build-excludes.sh` to only include crates failed >= 5 times
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- print-build-excludes.sh
+++ print-build-excludes.sh
@@ -3,4 +3,9 @@ set -euxo pipefail
 
 mkdir -p "$TARGET_ARCH"
 
-find "$TARGET_ARCH" -type f | sort --reverse | head -n 7 | xargs cat
+find "$TARGET_ARCH" -type f |
+    sort --reverse |
+    head -n 7 |
+    xargs cat |
+    # Print any excluded crate occured more than 5 times only once
+    awk '{cnt[$0]++; if (cnt[$0] == 5) print $0}'
