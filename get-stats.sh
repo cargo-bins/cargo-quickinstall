5860ad9f394600a0e30a0dca53cae5d080c7d35f
Jiahao XU <Jiahao_XU@outlook.com>
2023-03-07 14:32:00 +1100

    Make sure `get-stats.sh` never fail
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- get-stats.sh
+++ get-stats.sh
@@ -4,9 +4,10 @@ set -euo pipefail
 # Note that it is also possible to get stats from previous months via
 # /api/stats?year=2022&month=1
 
-exec curl \
+curl \
     --user-agent "cargo-quickinstall build pipeline (alsuren@gmail.com)" \
     --silent \
     --show-error \
     -XGET \
-    "https://warehouse-clerk-tmp.vercel.app/api/stats"
+    "https://warehouse-clerk-tmp.vercel.app/api/stats" ||
+    echo "Invalid json from stats server. Probably rate limit error. Skipping for now." >&2
