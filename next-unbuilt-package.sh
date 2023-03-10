bec02f422a99bb82df1594319d23bd45bf1fe7e0
Jiahao XU <Jiahao_XU@outlook.com>
2023-03-10 13:31:21 +1100

    Trigger exactly `CRATE_CHECK_LIMIT` builds for each target in `cronjob` (#193)
    Currently, `cronjob` only triggers one or two builds
    (way below `CRATE_CHECK_LIMIT` * num of targets)
    even if there are a lot of requested crates.
    
    This commit attempts to fix it with:
     - Read in `4 * $CRATE_CHECK_LIMIT` for `shuf` in `next-unbuilt-package.sh`
     - Filter all built and non-existent crates before `shuf` in `next-unbuilt-package.sh`
    
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- next-unbuilt-package.sh
+++ next-unbuilt-package.sh
@@ -26,6 +26,47 @@ if ! [[ $CRATE_CHECK_LIMIT =~ $re ]]; then
     CRATE_CHECK_LIMIT=20
 fi
 
+REPO="$(./get-repo.sh)"
+
+# see crawler policy: https://crates.io/policies
+curl_slowly() {
+    sleep 1 && curl --silent --show-error --user-agent "cargo-quickinstall build pipeline (alsuren@gmail.com)" "$@"
+}
+
+RESPONSE_DIR="$TEMPDIR/crates.io-responses/"
+mkdir -p "$RESPONSE_DIR"
+
+filter_already_built_crates() {
+    # shellcheck disable=SC2141
+    while IFS='$\n' read -r CRATE; do
+        # Fetch crates.io info
+        RESPONSE_FILENAME="$RESPONSE_DIR/$CRATE.json"
+        if [[ ! -f "$RESPONSE_FILENAME" ]]; then
+            if ! curl_slowly --location --fail "https://crates.io/api/v1/crates/${CRATE}" >"$RESPONSE_FILENAME"; then
+                echo "crates.io does not have ${CRATE}, continue" >&2
+                continue
+            fi
+        fi
+        is_valid_json="$(jq 'has("crate") and has("versions")' "$RESPONSE_FILENAME")"
+        if [ "$is_valid_json" != "true" ]; then
+            echo "crates.io does not have ${CRATE}, continue" >&2
+            continue
+        fi
+        VERSION="$(jq -r '.crate|.max_stable_version' "$RESPONSE_FILENAME")"
+        if [ -z "$VERSION" ] || [ "$VERSION" == "null" ]; then
+            echo "crates.io does not have ${CRATE}, continue" >&2
+            continue
+        fi
+
+        url="${REPO}/releases/download/${CRATE}-${VERSION}/${CRATE}-${VERSION}-${TARGET_ARCH}.tar.gz"
+        if curl_slowly --location --fail -I --output /dev/null "$url"; then
+            echo "${CRATE}-${VERSION}-${TARGET_ARCH}.tar.gz already uploaded. Keep going." >&2
+        else
+            echo "$CRATE"
+        fi
+    done
+}
+
 POPULAR_CRATES=$(
     if [ "$RECHECK" == 1 ]; then
         # always check quickinstall first for `make release`
@@ -42,10 +83,11 @@ POPULAR_CRATES=$(
         grep -v -e '^#' -e '^[[:space:]]*$' ./popular-crates.txt
     ) |
         # Remove duplicate lines, remove exclulded crates
-        # Limit max crate to check to 2 * CRATE_CHECK_LIMIT so that we can
+        # Limit max crate to check to 4 * CRATE_CHECK_LIMIT so that we can
         # randomly pick CRATE_CHECK_LIMIT from them, thus having different
         # POPULAR_CRATES in each run.
-        python3 ./dedup-and-exclude.py "${EXCLUDE_FILE?}" "$((2 * CRATE_CHECK_LIMIT))" |
+        python3 ./dedup-and-exclude.py "${EXCLUDE_FILE?}" "$((4 * CRATE_CHECK_LIMIT))" |
+        filter_already_built_crates |
         # -n specifies number of lines to output
         shuf -n "${CRATE_CHECK_LIMIT}" ||
         # If we don't find anything (package stopped being popular?)
@@ -53,20 +95,8 @@ POPULAR_CRATES=$(
         echo 'cargo-quickinstall'
 )
 
-# see crawler policy: https://crates.io/policies
-curl_slowly() {
-    sleep 1 && curl --silent --show-error --user-agent "cargo-quickinstall build pipeline (alsuren@gmail.com)" "$@"
-}
-
-REPO="$(./get-repo.sh)"
-
 for CRATE in $POPULAR_CRATES; do
-    RESPONSE_DIR="$TEMPDIR/crates.io-responses/"
     RESPONSE_FILENAME="$RESPONSE_DIR/$CRATE.json"
-    if [[ ! -f "$RESPONSE_FILENAME" ]]; then
-        mkdir -p "$RESPONSE_DIR"
-        curl_slowly --location --fail "https://crates.io/api/v1/crates/${CRATE}" >"$RESPONSE_FILENAME"
-    fi
     VERSION=$(jq -r '.crate|.max_stable_version' "$RESPONSE_FILENAME")
     FEATURES=$(
         jq -r ".versions[] | select(.num == \"$VERSION\") | .features | keys[]" "$RESPONSE_FILENAME" |
@@ -74,10 +104,6 @@ for CRATE in $POPULAR_CRATES; do
             paste -s -d ',' -
     )
 
-    if curl_slowly --location --fail -I --output /dev/null "${REPO}/releases/download/${CRATE}-${VERSION}/${CRATE}-${VERSION}-${TARGET_ARCH}.tar.gz"; then
-        echo "${CRATE}-${VERSION}-${TARGET_ARCH}.tar.gz already uploaded. Keep going." 1>&2
-    else
-        echo "${CRATE}-${VERSION}-${TARGET_ARCH}.tar.gz needs building" 1>&2
-        echo "{\"crate\": \"$CRATE\", \"version\": \"$VERSION\", \"target_arch\": \"$TARGET_ARCH\", \"features\": \"$FEATURES\"}"
-    fi
+    echo "${CRATE}-${VERSION}-${TARGET_ARCH}.tar.gz needs building" >&2
+    echo "{\"crate\": \"$CRATE\", \"version\": \"$VERSION\", \"target_arch\": \"$TARGET_ARCH\", \"features\": \"$FEATURES\"}"
 done
