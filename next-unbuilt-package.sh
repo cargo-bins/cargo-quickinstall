2f0ee5866eab2f530e44b6fd22bd1369d11e37b7
Jiahao XU <Jiahao_XU@outlook.com>
2023-04-27 23:10:45 +1000

    Enable any feature containing `static`
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- next-unbuilt-package.sh
+++ next-unbuilt-package.sh
@@ -106,7 +106,7 @@ for CRATE in $POPULAR_CRATES; do
     VERSION=$(jq -r '.crate|.max_stable_version' "$RESPONSE_FILENAME")
     FEATURES=$(
         jq -r ".versions[] | select(.num == \"$VERSION\") | .features | keys[]" "$RESPONSE_FILENAME" |
-            (grep "vendored" || true) |
+            (grep "vendored\|static" || true) |
             paste -s -d ',' -
     )
     NO_DEFAULT_FEATURES=""
