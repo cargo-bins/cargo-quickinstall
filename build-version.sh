5a5e40255dfa594e6999b7ff3881d1da2af9162f
Jiahao XU <Jiahao_XU@outlook.com>
2024-06-20 01:11:41 +1000

    Simplify `build-version.sh`
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- build-version.sh
+++ build-version.sh
@@ -80,5 +80,6 @@ fi
 #
 # BINARIES is a space-separated list of files, so it can't be quoted
 # shellcheck disable=SC2086
-tar --format=v7 -c $BINARIES | gzip -9 -c >"${TEMPDIR?}/${CRATE}-${VERSION}-${TARGET_ARCH}.tar.gz"
-ls "${TEMPDIR?}/${CRATE}-${VERSION}-${TARGET_ARCH}.tar.gz"
+artifact_path="${TEMPDIR?}/${CRATE}-${VERSION}-${TARGET_ARCH}.tar.gz"
+tar --format=v7 -c $BINARIES | gzip -9 -c >"$artifact_path"
+ls "$artifact_path"
