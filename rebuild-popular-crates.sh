88cf71dbacc60f3ff9650f1b290022d7620b97ff
David Laban <alsuren@gmail.com>
2024-09-06 22:34:39 +0100

    take indentation from main

--- rebuild-popular-crates.sh
+++ rebuild-popular-crates.sh
@@ -22,18 +22,18 @@ which uq || (
 
 function get_top() {
     curl --fail "https://lib.rs/$1" |
-    pup ':parent-of(:parent-of(:parent-of(.bin))) json{}' |
-    jq -r '.[] |
+        pup ':parent-of(:parent-of(:parent-of(.bin))) json{}' |
+        jq -r '.[] |
             (.children[1].children|map(select(.class == "downloads").title)[0]// "0 ")
             + ":" +
-    (.children[0].children[0].text)' |
-    sort -gr |
-    grep -v / |
-    grep -v ^0 |
-    head -n 100 |
-    tee /dev/fd/2 | # debugging goes to stderr
-    sed s/^.*://
-    
+            (.children[0].children[0].text)' |
+        sort -gr |
+        grep -v / |
+        grep -v ^0 |
+        head -n 100 |
+        tee /dev/fd/2 | # debugging goes to stderr
+        sed s/^.*://
+
     echo "done with $1" 1>&2
 }
 
