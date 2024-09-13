d42c9da5b3c3f4fd533f28f5db8ab3280965fa37
Jiahao XU <Jiahao_XU@outlook.com>
2024-09-13 18:10:31 +1000

    Refactor: Extract new fn `get_dfs`
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- cronjob_scripts/crates_io_popular_crates.py
+++ cronjob_scripts/crates_io_popular_crates.py
@@ -3,18 +3,20 @@ from __future__ import annotations
 import tarfile
 import warnings
 from pathlib import Path
+from typing import TypedDict
 
 import requests
 import polars as pl
 
 warnings.filterwarnings("ignore", message="Polars found a filename")
 
+
 class TarEntry:
     def __init__(self, tarball: tarfile.TarFile, member: tarfile.TarInfo):
         self.tarball = tarball
         self.member = member
 
-    def is_one_of_csvs_interested(self, csvs_to_extract: tuple[str, ...]) -> str | None:
+    def is_one_of_csvs_interested(self, csvs_to_extract: list[str]) -> str | None:
         if not self.member.isfile():
             return None
         for name in csvs_to_extract:
@@ -27,6 +29,7 @@ class TarEntry:
         assert stream
         return stream
 
+
 def download_tar_gz(url):
     response = requests.get(url, stream=True)
     response.raise_for_status()
@@ -35,31 +38,43 @@ def download_tar_gz(url):
         for member in tarball:
             yield TarEntry(tarball, member)
 
-def get_crates_io_popular_crates_inner(minimum_downloads=200000):
-    files_to_extract = ("crate_downloads.csv", "crates.csv", "default_versions.csv", "versions.csv")
+
+class Dfs(TypedDict):
+    crate_downloads: pl.LazyFrame
+    crates: pl.LazyFrame
+    default_versions: pl.LazyFrame
+    versions: pl.LazyFrame
+
+
+def get_dfs() -> Dfs:
+    files_to_extract = ["crate_downloads.csv", "crates.csv", "default_versions.csv", "versions.csv"]
     files_extracted = 0
     dfs = {}
     for entry in download_tar_gz('https://static.crates.io/db-dump.tar.gz'):
         name = entry.is_one_of_csvs_interested(files_to_extract)
         if name:
-            dfs[name] = pl.scan_csv(entry.get_file_stream())
+            dfs[name[:-4]] = pl.scan_csv(entry.get_file_stream())
             files_extracted += 1
             if files_extracted == len(files_to_extract):
-                return (
-                    dfs["crate_downloads.csv"]
-                    .join(dfs["crates.csv"].select("id", "name"), left_on="crate_id", right_on="id")
-                    .join(dfs["default_versions.csv"], on="crate_id")
-                    .join(
-                        dfs["versions.csv"].select("id", "crate_id", "yanked", "bin_names"),
-                        left_on=("crate_id", "version_id"),
-                        right_on=("crate_id", "id"),
-                    )
-                    .filter(pl.col("bin_names") != "{}", pl.col("yanked") == "f")
-                    .filter(pl.col("downloads") > minimum_downloads)
-                    .select("name")
-                )
+                return Dfs(**dfs)
 
     raise RuntimeError(f"Failed to find all csvs {files_to_extract}")
+
+def get_crates_io_popular_crates_inner(minimum_downloads=200000):
+    dfs = get_dfs()
+    return (
+        dfs["crate_downloads"]
+        .join(dfs["crates"].select("id", "name"), left_on="crate_id", right_on="id")
+        .join(dfs["default_versions"], on="crate_id")
+        .join(
+            dfs["versions"].select("id", "crate_id", "yanked", "bin_names"),
+            left_on=("crate_id", "version_id"),
+            right_on=("crate_id", "id"),
+        )
+        .filter(pl.col("bin_names") != "{}", pl.col("yanked") == "f")
+        .filter(pl.col("downloads") > minimum_downloads)
+        .select("name")
+    )
 
 
 def get_crates_io_popular_crates(minimum_downloads=200000):
